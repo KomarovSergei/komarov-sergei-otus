2847e1cc08a6955cb0ccadefe3b55f6a07eb00b5
Misha Komarov <Misha140699@mail.ru>
2019-05-11 22:29:06 +0300

    add skeleton for hw

---
+++ javascript/lesson-10/src/index.js
@@ -0,0 +1,122 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+const products = [{
+  id: 1,
+  price: 2.5,
+  name: 'test product 1',
+  size: 9,
+  color: 'wite',
+  count: 1
+}, {
+  id: 2,
+  price: 4.5,
+  name: 'test product 2',
+  size: 15,
+  color: 'red',
+  count: 2
+}, {
+  id: 3,
+  price: 5.5,
+  name: 'test product 3',
+  size: 25,
+  color: 'blue',
+  count: 1
+}]
+
+const customers = [{
+  id: '1',
+  name: 'customer1',
+  payment: 'payment1',
+  cart: '1'
+}, {
+  id: '2',
+  name: 'customer2',
+  payment: 'payment2',
+  cart: '2'
+}, {
+  id: '3',
+  name: 'customer3',
+  payment: 'payment3',
+  cart: '3'
+}]
+
+const carts = [{
+  id: '1',
+  product: '1',
+  customer: '1'
+}, {
+  id: '2',
+  product: '2',
+  customer: '2'
+}, {
+  id: '3',
+  product: '3',
+  customer: '3'
+}]
+
+const typeDefs = `
+  type Query {
+    products: [Product!] !
+    customers: [Customer!]!
+    carts: [Cart!]!
+  }
+
+  type Product {
+    id: ID!
+    price: Float!
+    name: String!
+    size: Int!
+    color: String!
+    count: Int!
+  }
+
+  type Customer {
+    id: ID!
+    name: String!
+    payment: String!
+    cart: Cart!
+  }
+
+  type Cart {
+    id: ID
+    product: Product
+    customer: Customer
+  }
+`
+
+const resolvers = {
+  Query: {
+    products(parent, args, cnt, info) {
+      return products
+    },
+    customers(parent, args, cnt, info) {
+      return customers
+    },
+    carts(parent, args, cnt, info) {
+      return carts
+    }
+  }, 
+  Customer: {
+    cart(parent, args, ctx, info) {
+      return carts.filter(cart => {
+        cart.id === parent.cart
+      })
+    }
+  },
+  Cart: {
+    product(parent, args, ctx, info) {
+      console.dir(parent)
+      return products.filter(product => parent.product === product.id)
+    },
+    customer(parent, args, ctx, info) {
+      return customers.filter(customer => parent.customer === customer.id)
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers
+})
+
+server.start(() => console.log('The server is up!'))
