0029343d43c8d251d37340e19ca434b811e91fe5
Sergei Komarov <komarovs33@mail.ru>
2019-05-13 19:49:57 +0300

    del unused variable

--- javascript/lesson-10/src/resolvers/Cart.js
+++ javascript/lesson-10/src/resolvers/Cart.js
@@ -3,7 +3,6 @@ const Cart = {
     db
   }, info) {
     let outArr = []
-    let temp = []
     if(typeof parent.order === 'object' && Object.keys(parent.order).length > 1) {
       for(let i = 0; i < parent.order.length; i++) {
         outArr = outArr.concat(db.orders.filter(order => parent.order[i] === order.id))
