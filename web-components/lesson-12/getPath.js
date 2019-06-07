f303fa1bc92ac5f4db7daa32808c688b04f9d02f
Sergei Komarov <komarovsmail33@gmail.com>
2019-06-07 13:16:44 +0300

    fix for pr

--- web-components/lesson-12/getPath.js
+++ web-components/lesson-12/getPath.js
@@ -1,15 +1,23 @@
 function runPath(elem) {
-    const elemIn = elem || window.document.getElementById('find_me')
-    let arrPath = []
+  const elemIn = elem || window.document.getElementById('find_me');
+  let arrPath = [];
+  const outString = "";
 
-    function getPath(elemIn, arrPath) {
+  const getPath = (elemIn, arrPath) => {
     if (elemIn.parentElement) {
-        arrPath.push(elemIn.localName)
-        getPath(elemIn.parentElement, arrPath)
+      arrPath.push(elemIn.localName);
+      getPath(elemIn.parentElement, arrPath);
     }
-    }
-    getPath(elemIn, arrPath)
-    arrPath = arrPath.reverse()
+  };
+  
+  getPath(elemIn, arrPath);
+  arrPath = arrPath.reverse();
 
-    return arrPath.join(' ')
+  if(arrPath.length > 0) {
+    if(document.querySelectorAll(arrPath.join(' ')).length > 1) {
+      return document.querySelectorAll(arrPath.join(' '))[0];
+    }
+  }
+
+  return arrPath.join(' ');
 }
